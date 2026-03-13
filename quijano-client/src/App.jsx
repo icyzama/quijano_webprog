ce5c9b05abee92fde5e02096d9e5277a22d2a165
icyzama <reynaldrodriguezquijano@gmail.com>
2026-03-13 18:19:57 +0800

    initial commit

---
+++ quijano-client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: [Reynald R. Quijano]<br />
+          Email: [quijanorrq@gmail.com]<br />
+          Reynald is an aspiring cloud and data engineer and a junior at National University – Manila, with hands-on experience in UI/UX design and frontend development. He is skilled in Figma, React JS, HTML, CSS, and JavaScript, with projects and hackathon experience. Passionate about technology, innovation, and leadership, Reynald thrives in collaborative problem-solving and continuously seeks opportunities to grow and create impactful technology solutions.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
