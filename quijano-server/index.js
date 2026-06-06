6b5ce3bbf33607e46d3403c77bff4c6c42c7e3eb
icyzama <reynaldrodriguezquijano@gmail.com>
2026-06-06 15:08:24 +0800

    Fix Vercel backend entrypoint

--- quijano-server/index.js
+++ quijano-server/index.js
@@ -57,7 +57,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
+if (!process.env.VERCEL) {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
